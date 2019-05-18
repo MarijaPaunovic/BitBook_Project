8c4ddf532ac0d1bedc21cbade72d452a93b57ad8
Marija Paunović <marijapaunovicdesign@gmail.com>
2019-05-18 17:56:47 +0200

    Update constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -5,6 +5,7 @@ export const API_BASE_URL = "http://book-api-dev.hypetech.xyz";
 export const HEADERS_API = {
     headers: {
         "content-type": "application/json",
-        "x-api-key": "B1tD3V"
+        "x-api-key": "B1tD3V",
+        "X-From": "Netlify"
     }
 }
