9900d88950ba45c2fb688776b1e8ea6955d8137e
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2019-05-19 16:49:07 +0200

    Updates

---
+++ precache-manifest.0250c1dfc4d6634449b7ef680465604a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "809ea8288953845a974d",
+    "url": "/BitBook_Project/static/js/runtime~main.dffbac11.js"
+  },
+  {
+    "revision": "16cc7bf0d29ec0c49c1a",
+    "url": "/BitBook_Project/static/js/main.4ee39147.chunk.js"
+  },
+  {
+    "revision": "51b8def831ce8112419d",
+    "url": "/BitBook_Project/static/js/2.4c5674ed.chunk.js"
+  },
+  {
+    "revision": "16cc7bf0d29ec0c49c1a",
+    "url": "/BitBook_Project/static/css/main.2fecc744.chunk.css"
+  },
+  {
+    "revision": "4db4ccd17494b8506c14c910de845620",
+    "url": "/BitBook_Project/index.html"
+  }
+];
